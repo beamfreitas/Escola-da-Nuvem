e66fb6d7575111075dd04e48336d3ae7baafab9b
beamfreitas <144755141+beamfreitas@users.noreply.github.com>
2025-08-03 14:00:58 -0300

    01

---
+++ pratica-01/calculadora_de_preço_total.py
@@ -0,0 +1,19 @@
+"""4- Calculadora de Preço Total
+* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+* Nome do produto: "Cadeira Infantil"
+* Preço unitário: R$ 12.40
+* Quantidade: 3
+O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final."""
+
+NomeDoProduto = "cadeira infantil"
+PrecoUnitario = 12.40
+quantidade = 3
+
+PrecoTotal = PrecoUnitario * quantidade
+
+print("Nome do Produto:", NomeDoProduto)
+print("Preço Unitário: R$", PrecoUnitario)
+print("Quantidade:", quantidade)
+
+print("Preço Total: R$", PrecoTotal)
