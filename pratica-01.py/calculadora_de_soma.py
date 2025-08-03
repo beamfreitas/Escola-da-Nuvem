55a9e395bc9e78b5d21a5947dde0432927f577a9
beamfreitas <144755141+beamfreitas@users.noreply.github.com>
2025-08-03 13:28:45 -0300

    pratica-01

---
+++ pratica-01.py/calculadora_de_soma.py
@@ -0,0 +1,11 @@
+# 2- Calculadora de Soma
+# * Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.
+
+numero1 = 12
+numero2 = 14
+print(numero1+numero2)
+
+soma = numero1 + numero2
+print(soma)
+
+print(f"a soma de {numero1} e {numero2} é {soma}")
